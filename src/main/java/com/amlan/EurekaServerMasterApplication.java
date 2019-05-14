28e31c033c24469a463c55baa6ac916446b9c760
amlan2 <50303264+amlan2@users.noreply.github.com>
2019-05-14 15:34:13 +0530

    xf

--- src/main/java/com/amlan/EurekaServerMasterApplication.java
+++ src/main/java/com/amlan/EurekaServerMasterApplication.java
@@ -2,7 +2,6 @@ package com.amlan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
